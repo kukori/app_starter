64af0ca44bd7824eb643b0e1a6aa9fd6c8cf9c0c
attilav <attila.varjas@gmail.com>
2020-05-07 15:13:31 +0700

    Adding the client

--- server.js
+++ server.js
@@ -65,6 +65,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/users', users);
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 // Error handler middleware
 app.use(errorHandler);
 
